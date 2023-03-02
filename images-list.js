92e0e564807e96489ed7cfe2dc8de47524a24fed
Vadimus <vesvadvlad1@gmail.com>
2023-03-02 11:58:24 +0300

    загрузил все картинки с переводами

--- images-list.js
+++ images-list.js
@@ -201,6 +201,209 @@ window[sharedNames.images] = [
         src: './images/collages/masha/10.png',
         text: `get housing, open a maternity home, return prizes`
     },
+
+    /// NIKITA
+
+    {
+        src: './images/collages/nikita/обработанныекартинки/20230203-152824(1).jpeg',
+        text: `HORSE IN A COAT<br>
+        <br>
+    just for<br>
+    JOY
+    `},
+    {
+        src: './images/collages/nikita/обработанныекартинки/20230203-152824.jpeg',
+        text: `A MILLION A MONTH<br>
+    unaffordable for the blind<br>`},
+    {
+        src: './images/collages/nikita/обработанныекартинки/20230203-152924(1).jpeg',
+        text: `«men are like pies: either with cabbage, or with eggs»`
+    },
+    {
+        src: './images/collages/nikita/обработанныекартинки/20230203-152924.jpeg',
+        text: `main<br>
+    ingredient:<br>
+    how<br>
+    to get money ….<br>
+    <br>
+    <br>
+    Money flowed out`
+    },
+    {
+        src: './images/collages/nikita/обработанныекартинки/20230203-153041(1).jpeg',
+        text: `<br>
+        <br>
+    every day<br>
+    rises and falls<br>`
+    },
+    {
+        src: './images/collages/nikita/обработанныекартинки/20230203-153041.jpeg',
+        text: `<br>
+    retirement witout worries?<br>
+    <br>
+    They're fine without it!<br>
+    <br>
+    And nothing's gonna happen to us!<br>
+    `},
+    {
+        src: './images/collages/nikita/обработанныекартинки/20230203-153212(1).jpeg',
+        text: `<br>
+     GAY-ATTACK<br>
+     <br>
+    AGAINST ALL ODDS<br>
+    `},
+    {
+        src: './images/collages/nikita/обработанныекартинки/20230203-153212.jpeg',
+        text: `<br>
+        <br>
+    GAY-ATTACK<br>
+    COMES OUT<br>
+    OF SHADOWS<br>
+     LGBT  RELEASED <br>
+    `},
+    {
+        src: './images/collages/nikita/обработанныекартинки/20230203-153400.jpeg',
+        text: `<br>
+    LET SCANDAL HAPPEN<br>`
+    },
+
+    /// Vava
+
+    {
+        src: './images/collages/vava/20230203-15415(1).jpg', text: `All strings broke?<br>
+    Do you think being healthy is sad?<br>
+    Revelation is an expensive thing.` },
+    { src: './images/collages/vava/20230203-154159.jpg', text: `HOW TO HEAL` },
+    {
+        src: './images/collages/vava/20230203-154303.jpg', text: `Enough drinking blood….<br>
+     Sex is within our power`},
+    {
+        src: './images/collages/vava/20230203-154410.jpg', text: `From the perspective of 49 cents <br>
+    it is worth to distinguish ageing from illness `},
+    { src: './images/collages/vava/20230203-154523.jpg', text: `Bathhouses will float` },
+    { src: './images/collages/vava/20230203-154631.jpg', text: `Great loss` },
+    { src: './images/collages/vava/20230203-154747.jpg', text: `I’m an owl, you’re…pigeon` },
+    { src: './images/collages/vava/20230203-154921.jpg', text: `It’s not far from abruption` },
+    { src: './images/collages/vava/20230203-155009.jpg', text: `A minute for self-discovery` },
+    {
+        src: './images/collages/vava/20230203-161842.jpg', text: `Feminine magic<br>
+     Accept and destroy`},
+
+    /// Liza
+
+    {
+        src: "./images/collages/liza/IMG_1718.PNG",
+        text: "\nFlaming\n Secret\n\n"
+    },
+    {
+        src: "./images/collages/liza/IMG_1733.PNG",
+        text: "\nGREEN\n\n"
+    },
+    {
+        src: "./images/collages/liza/IMG_1744.PNG",
+        text: "\nmusic\nstars\n\nsecret of your charms\n\n"
+    },
+    {
+        src: "./images/collages/liza/IMG_1745.PNG",
+        text: "\nTEMPTATION\n\n"
+    },
+    {
+        src: "./images/collages/liza/IMG_1746.PNG",
+        text: "\nFUTURE\n\n"
+    },
+    {
+        src: "./images/collages/liza/IMG_1747.PNG",
+        text: "\nAre you enduring that pain?\n\n"
+    },
+    {
+        src: "./images/collages/liza/IMG_1749.PNG",
+        text: "\nWhere to start?\n- Kindness\n\n"
+    },
+    {
+        src: "./images/collages/liza/IMG_1750.PNG",
+        text: "\nFeast of\nImagination\n\n"
+    },
+    {
+        src: "./images/collages/liza/IMG_1751.PNG",
+        text: "\nThere is never too much\n\n"
+    },
+    {
+        src: "./images/collages/liza/IMG_1752.PNG",
+        text: "\nYou’ll find an interesting job\n\n"
+    },
+    {
+        src: "./images/collages/liza/IMG_1753.PNG",
+        text: "\nFrom distant wanderings\n\n"
+    },
+    {
+        src: "./images/collages/liza/IMG_1754.PNG",
+        text: "\nAppetite comes with eating\n\n"
+    },
+    {
+        src: "./images/collages/liza/IMG_1755.PNG",
+        text: "\ntwo\n worlds\n\n"
+    },
+    {
+        src: "./images/collages/liza/IMG_1757.PNG",
+        text: "\nNext\n Mission possible\n\n"
+    },
+    {
+        src: "./images/collages/liza/IMG_1758.PNG",
+        text: "\nI THINK\nYOU MADE A MISTAKE\n\n(opinion)\n\n"
+    },
+    {
+        src: "./images/collages/liza/IMG_1759.PNG",
+        text: "\n\nWith all the comforts\nEverything for the good of man\n\nsex\n\n"
+    },
+    {
+        src: "./images/collages/liza/IMG_1760.PNG",
+        text: "\npinholed\ndream\n"
+    },
+
+    /// ALINA_D
+
+    {
+        src: "./images/collages/alina_d/Скан_20230207(2).jpg",
+        text: `long journey
+
+        the first pathway
+
+        space high`
+    },
+    {
+        src: './images/collages/alina_d/Скан_20230208 (2).jpg',
+        text: `How to fight with aggression?
+            Vasily Simonov 
+            RECOMMENDED:
+               “AESTHETIC 
+            CAN
+            HEAL PEOPLE”`
+    },
+    {
+        src: './images/collages/alina_d/IMG_4809.JPG',
+        text: `man 
+            defender
+            feelings
+            passion
+
+            even better 
+            GIVE ME TWO!
+
+            NOTHING
+            EXTRA`
+    },
+    {
+        src: "./images/collages/alina_d/IMG_4810.JPG",
+        text: `Troubles?
+
+            Fuck 
+            them!
+
+            let’s not get exchanged for trifles. 
+            problems are solvable!`
+    }
+
+
 ].map(item => ({
     image: item.src,
     html: item.html || '<span style="position: absolute; top: 30%; left: 50%; transform: translateX(-50%);">' + item.text.trim().split('\n').join('<br>') + '</span>'
